858fd13aee35c59a86b67f67b47c708bb3e1acf6
root <root@LAPTOP-H78VOC8K.localdomain>
2017-12-05 21:29:19 -0500

    merged pats

--- src/App.js
+++ src/App.js
@@ -8,10 +8,38 @@ import JsonTable from 'react-json-table';
 import topimage from './Capture.PNG';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+var firebase = require('firebase');
+var config = {
+	apiKey: "AIzaSyAOIrw-yoWwGlIUENrT9JYhm4VFNCUfhyE",
+	databaseURL: "https://fir-demo-79a1f.firebaseio.com/",
+	storageBucket: "",
+  };
+var app = firebase.initializeApp(config);
+var list = ["A", "B", "C", "D", "E"];
+for(var i in list){
+
+	//console.log(app.database().ref(list[i]).update({team1player1:"", team1player2:"", team2player1:"", team2player2:"", team1score:0, team2score:0}));
+}
 
 class App extends Component {
 	
-	
+	setup(){
+		var parent = this;
+		app.database().ref(parent.state.room == ""?"A":parent.state.room).on('value', function(dataSnapshot) { 
+			console.log("CALLED" );
+			console.log("data:", dataSnapshot.val());
+			var x = dataSnapshot.val();
+			//parent.state.setState(dataSnapshot.val().team1player1
+			parent.setState(function(prevstate, props){return {team1player1:x.team1player1,
+				team1player2:x.team1player2,
+				team2player1:x.team2player1,
+				team2player2:x.team2player2,
+				team1score:x.team1score,
+				team2score:x.team2score
+				}})	
+		
+		})
+	}
 
 constructor(props) {
   super(props);
@@ -23,12 +51,15 @@ constructor(props) {
   gameslist:[],
     color: props.initialColor,
 	heightstring: "800px",
+	allgames:[],
+	pairings:[],
 	team2score: 0,
 	team1score: 0,
 	team1player1: "",
 	team2player2: "",
 	team1player2: "",
-	team2player1: ""
+	team2player1: "",
+	room: ""
   };
   var myparent = this;
   // names
@@ -50,14 +81,14 @@ constructor(props) {
 
 		});
 // games
-
+this.setup();
 
 }
 
 
   render() {
   var parent = this;
-  console.log(this.items)
+  
     return (
       <div className="App" style={{"background-color":"#2e9858", "height" :this.state.heightstring}}>
 	  
@@ -67,6 +98,7 @@ constructor(props) {
 		<TabList>
 			<Tab>Players</Tab>
 			<Tab>Games</Tab>
+			<Tab>Tournament</Tab>
 		</TabList>
 		<TabPanel>
 		<Tabs>
@@ -100,7 +132,7 @@ constructor(props) {
 					} >Submit</button>
 				</div>
 			</TabPanel>
-			<TabPanel style={{width:"800px", margin:0 }}>
+			<TabPanel style={{width:"50%", margin:0 }}>
 				<div id="allGameTable" style={{width:"800px", "margin-left":"600px" }}>
 				
 					<JsonTable rows={this.state.items} style={{"background-color":"#2e9858", "height" :"100%", "margin-left":"600px"}}/>
@@ -261,47 +293,196 @@ constructor(props) {
 				<input type="text" id="deleteGameQuery"/>
 			</TabPanel>
 			<TabPanel>
+			<Dropdown options={list.map(function(item){
+				return {value:item, label:item}
+			})} onChange={function(event){
+				app.database().ref(parent.state.room == ""?"A":parent.state.room).off('value', function(dataSnapshot) { 
+					console.log("CALLED" );
+					console.log("data:", dataSnapshot.val());
+					var x = dataSnapshot.val();
+					//parent.state.setState(dataSnapshot.val().team1player1
+					parent.setState(function(prevstate, props){return {team1player1:x.team1player1,
+						team1player2:x.team1player2,
+						team2player1:x.team2player1,
+						team2player2:x.team2player2,
+						team1score:x.team1score,
+						team2score:x.team2score
+						}})	
+				
+				})
+				parent.setState(function(prevstate, props){return {room: event.value}})
+				app.database().ref(event.value).on('value', function(dataSnapshot) { 
+					console.log("CALLED" );
+					console.log("data:", dataSnapshot.val());
+					var x = dataSnapshot.val();
+					//parent.state.setState(dataSnapshot.val().team1player1
+					parent.setState(function(prevstate, props){return {team1player1:x.team1player1,
+						team1player2:x.team1player2,
+						team2player1:x.team2player1,
+						team2player2:x.team2player2,
+						team1score:x.team1score,
+						team2score:x.team2score
+						}})	
+				
+				})
+				app.database().ref(event.value).once('value', function(dataSnapshot) { 
+					console.log("CALLED" );
+					console.log("data:", dataSnapshot.val());
+					var x = dataSnapshot.val();
+					//parent.state.setState(dataSnapshot.val().team1player1
+					parent.setState(function(prevstate, props){return {team1player1:x.team1player1,
+						team1player2:x.team1player2,
+						team2player1:x.team2player1,
+						team2player2:x.team2player2,
+						team1score:x.team1score,
+						team2score:x.team2score
+						}})	
+				
+				})
+				
+				}} value={parent.state.room} placeholder="Select Room" />
 			<Dropdown options={parent.state.items.map(function(item){
 				return {value:item.Netid, label:item.Name}
-			})} onChange={this._onSelect} value={""} placeholder="Select Team 1 Player 1" />
+			})} onChange={function(event){parent.setState(function(prevstate, props){return {team1player1: event.value}});app.database().ref(parent.state.room).update({team1player1:event.value})}} value={parent.state.team1player1} placeholder="Select Team 1 Player 1" />
 				<Dropdown options={parent.state.items.map(function(item){
 				return {value:item.Netid, label:item.Name}
-			})} onChange={this._onSelect} value={""} placeholder="Select Team 1 Player 2" />
+			})} onChange={function(event){parent.setState(function(prevstate, props){return {team1player2: event.value}}); app.database().ref(parent.state.room).update({team1player2:event.value})}} value={parent.state.team1player2} placeholder="Select Team 1 Player 2" />
 				
 				<div id="score1">
 					<button className="decScore" onClick={function(){
-							parent.setState
-							(function(prevstate, props){return {team1score: prevstate.team1score-1}})
+						app.database().ref(parent.state.room).update({team1score:parent.state.team1score-1});
+							//parent.setState(function(prevstate, props){return {team1score: prevstate.team1score-1}})
 					}}>-</button>
 					<input type="number" id="team1Score" value={parent.state.team1score} min="0" max="13"></input>
 					<button className="incScore" onClick={function(){
-							parent.setState
-							(function(prevstate, props){return {team1score: prevstate.team1score+1}})
+						app.database().ref(parent.state.room).update({team1score:parent.state.team1score+1});
+							//parent.setState(function(prevstate, props){return {team1score: prevstate.team1score+1}})
 					}}>+</button>
 				</div>
 				<div>
 					<button className="decScore" onClick={
 					function(){
-							parent.setState
-							(function(prevstate, props){return {team2score: prevstate.team2score-1}})
+						app.database().ref(parent.state.room).update({team2score:parent.state.team2score-1});
+							//parent.setState(function(prevstate, props){return {team2score: prevstate.team2score-1}})
 					}
 					}>-</button>
 					<input type="number" id="team2Score" value={parent.state.team2score} min="0" max="13"></input>
 					<button className="incScore" onClick={function(){
-							parent.setState
-							(function(prevstate, props){return {team2score: prevstate.team2score+1}})
+							app.database().ref(parent.state.room).update({team2score:parent.state.team2score+1});
+							//parent.setState(function(prevstate, props){return {team2score: prevstate.team2score+1}});
 					}}>+</button>
 				</div>
 				<Dropdown options={parent.state.items.map(function(item){
 				return {value:item.Netid, label:item.Name}
-			})} onChange={this._onSelect} value={""} placeholder="Select Team 2 Player 1" />
+			})} onChange={function(event){parent.setState(function(prevstate, props){return {team2player1: event.value}});app.database().ref(parent.state.room).update({team2player1:event.value})}} value={parent.state.team2player1} placeholder="Select Team 2 Player 1" />
 				<Dropdown options={parent.state.items.map(function(item){
 				return {value:item.Netid, label:item.Name}
-			})} onChange={function(event){console.log()}} value={""} placeholder="Select Team 2 Player 2" />
+			})} onChange={function(event){parent.setState(function(prevstate, props){return {team2player2: event.value}}); app.database().ref(parent.state.room).update({team2player2:event.value})}} value={parent.state.team2player2} placeholder="Select Team 2 Player 2" />
+			<div>
+				
+			</div>
 			</TabPanel>
 			</Tabs>
 		</TabPanel>
+		<TabPanel>
+		<button id="makeTourny" onClick={
+		function(text){
+			var data = {
+			"netid":"asdf"
+			}
+			console.log("tourny")
+			$.ajax({
+				type: "POST",
+				data: JSON.stringify(data),
+				url: "https://test-cse30246.azurewebsites.net/api/HttpTriggerCSharp2?code=YvOorCUyGOMa8p6HXM1R78DSK74lUMhOK82N2zaxZ6abBOF6HiW1TQ==",
+				success: function (text) {
+						parent.setState(
+							function(prevstate, props){
+								return {allgames:text.map(function(i){
+									return i
+										})
+								}
+							})
 
+
+					var playerGameStats = {}; //key = netid, value 0 = sum, 1 = num games
+					var playerGameCount = {};
+					var playerElo = {};
+					var names = new Set();
+					for (var i = 0; i<parent.state.allgames.length; i++){
+						playerGameStats[parent.state.allgames[i][1]] = 0;
+						playerGameStats[parent.state.allgames[i][2]] = 0;
+						playerGameStats[parent.state.allgames[i][4]] = 0;
+						playerGameStats[parent.state.allgames[i][5]] = 0;
+						playerGameCount[parent.state.allgames[i][1]] = 0;
+						playerGameCount[parent.state.allgames[i][2]] = 0;
+						playerGameCount[parent.state.allgames[i][4]] = 0;
+						playerGameCount[parent.state.allgames[i][5]] = 0;
+						names.add(parent.state.allgames[i][1]);
+						names.add(parent.state.allgames[i][2]);
+						names.add(parent.state.allgames[i][4]);
+						names.add(parent.state.allgames[i][5]);
+					}
+					for (var i = 0; i<parent.state.allgames.length; i++){
+						//add players opponents elo
+						//figure out who wins and add/subtrack 400 to that sum
+						if (parent.state.allgames[i][3] > parent.state.allgames[i][6]){
+							playerGameStats[parent.state.allgames[i][1]] += 1900;
+							playerGameStats[parent.state.allgames[i][2]] += 1900;
+							playerGameStats[parent.state.allgames[i][4]] += 1100;
+							playerGameStats[parent.state.allgames[i][5]] += 1100;
+						}
+						else {
+							playerGameStats[parent.state.allgames[i][4]] += 1900;
+							playerGameStats[parent.state.allgames[i][5]] += 1900;
+							playerGameStats[parent.state.allgames[i][1]] += 1100;
+							playerGameStats[parent.state.allgames[i][2]] += 1100;
+						}
+						playerGameCount[parent.state.allgames[i][1]]++;
+						playerGameCount[parent.state.allgames[i][2]]++;
+						playerGameCount[parent.state.allgames[i][4]]++;
+						playerGameCount[parent.state.allgames[i][5]]++;
+					}
+					for (let item of names){
+						playerElo[item] = playerGameStats[item] / playerGameCount[item];
+					}
+					var sortedPlay = [];
+					var revSortedPlay = [];
+					for (var player in playerElo) {
+    				sortedPlay.push([player, playerElo[player]]);
+						revSortedPlay.push([player, playerElo[player]]);
+					}
+
+					sortedPlay.sort(function(a, b) {
+    				return a[1] - b[1];
+					});
+					revSortedPlay.sort(function(a, b) {
+    				return b[1] - a[1];
+					});
+					var emptyPairings = [];
+					for (var i = 0; i < sortedPlay.length/2; i++){
+						emptyPairings.push([sortedPlay[i][0], revSortedPlay[i][0]]);
+					}
+
+					parent.setState(
+						function(prevstate, props){
+							return {pairings:emptyPairings.map(function(i){
+								return {"Team Member 1":i[0], "Team Member 2": i[1]}
+									})
+							}
+						})
+					console.log(parent.state.pairings);
+
+					},
+				headers: {"Content-Type": "application/json"}
+				}
+			);
+			}
+		}>Make Tournament</button>
+		<center><JsonTable rows={this.state.pairings} style={{"margin-left":"600px" }}/></center>
+
+
+		</TabPanel>
 	</Tabs>
       </div>
     );
